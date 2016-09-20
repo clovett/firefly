0851ff2a7089052dd9857c0ec88750a03c8c7e8e
SmileyBone <=>
2016-09-20 12:53:11 -0700

    started revamping the connection methods and structure

--- scripts/master.py
+++ scripts/master.py
@@ -1,4 +1,5 @@
 from networking import network
+import threading
 
 class master(object):
 
@@ -6,8 +7,17 @@ class master(object):
         self.network = network(is_master = True)
         self.network.connect()
 
+    """
+    Run a simple loop for debugging
+    """
+    def loop(self):
+        while(1):
+            #check for new connections
+            self.network.connect()
+
+
+
 if __name__ == "__main__":
     print "running main program"
     m = master()
-    while(1):
-        m.network.connect()
+    m.loop()
--- scripts/networking.py
+++ scripts/networking.py
@@ -1,16 +1,44 @@
-from socket import *
+import socket
 from Queue import Queue
 from threading import Thread
 import time
 
+class connection(object):
+    def __init__(self, tcp_conn):
+        self._tcp_conn = tcp_conn
+        self._incoming_queue = Queue()
+        self._closed = False
+
+    def _listen_thread(self):
+        while not self._closed:
+            data = self._tcp_conn.recv(4096)
+            self._incoming_queue.put(data)
+
+    def send(self, data):
+        return self._tcp_conn.send(data)
+
+    def receive(self):
+        data = None
+        try:
+            data = self._incoming_queue.get_nowait()
+        except socket.empty:
+            data = None
+        return data
+
+    def close(self):
+        self._closed = True
+        self._tcp_conn.close()
+
+
 class network(object):
-
-    MASTER_LISTEN_PORT = 8008
-
     def __init__(self, is_master = False):
         self._is_master = is_master
         self._connection_list = []
         self._message_queue = Queue()
+
+        if self._is_master:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.listen(5)
 
     """
     return true if connected to at least one other instance of network
@@ -19,7 +47,8 @@ class network(object):
         return len(self._connection_list) > 0
 
     """
-    Block until connected to master, or if master, until at least one node connects
+    Block until connected to master, or if master, do a non-blocking check for
+    nodes that are note yet in the connection list.
     """
     def connect(self):
         if self._is_master:
@@ -27,23 +56,54 @@ class network(object):
         else:
             self._node_connect()
 
-    def _master_connect(self):
-        sock = socket(AF_INET, SOCK_DGRAM)
-        sock.bind(('', self.MASTER_LISTEN_PORT))
-        data, addr = sock.recvfrom(1024)
+    """
+    send a message with the master server information
+    """
+    def master_broadcast(self):
+        pass
 
-        print "got data:", data, "from ", addr
+
+    def _master_connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('', self.MASTER_LISTEN_PORT))
+        sock.settimeout(1)
+
+        try:
+            data, (ip, port) = sock.recvfrom(1024)
+        except socket.timeout:
+            pass
+        else:
+            if data == self.NODE_CONNECTION_MSG:
+                tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                tcp_sock.connect((ip, self.NODE_SERVER_PORT))
+
+                #set the connection for that ip to the tcp connection
+                print "new connection", tcp_sock
+                self._connection_list[ip] = tcp_sock
+        finally:
+            sock.close()
+
 
     def _node_connect(self):
         if not self.connected():
-            sock = socket(AF_INET, SOCK_DGRAM)
-            sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+            udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+            tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            node_addr = tcp_sock.bind(('', self.NODE_SERVER_PORT))
+            tcp_sock.listen(5)
+            tcp_sock.settimeout(1)
 
             done = False
             while not done:
-                time.sleep(1)
-                sock.sendto("hello", ('255.255.255.255', self.MASTER_LISTEN_PORT))
-
+                udp_sock.sendto(self.NODE_CONNECTION_MSG, ('255.255.255.255', self.MASTER_LISTEN_PORT))
+                try:
+                    sock, (ip, port) = tcp_sock.accept()
+                except socket.timeout:
+                    pass
+                else:
+                    done = True
+                    self._connection_list[ip] = tcp_sock
 
     """
     Send the message to the master, or if is_master send to the given destination.
