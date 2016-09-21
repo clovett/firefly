afd452d626e2fa3eda90fbfc2bede5fcc6d23209
SmileyBone <=>
2016-09-20 18:45:43 -0700

    networking module mostly d finished

--- scripts/sim_module.py
+++ scripts/sim_module.py
@@ -5,26 +5,26 @@ class sim_node(object):
     def __init__(self):
         self.client = Client()
 
-start_time = time.time()
-def main(sim):
+def main(sim, start_time):
     data = None
     if sim.client.connected():
         data = sim.client.receive()
         if data is not None:
             print data
 
-        if time.time() - start_time > 1:
+        if time.time() - start_time > 0.1:
             start_time = time.time()
             msg = "boop"
             sim.client.send(msg)
 
 if __name__ == "__main__":
+    start_time = time.time()
     print "starting sim_node"
     sim = sim_node()
     done = False
     while not done:
         try:
-            main(sim)
+            main(sim, start_time)
         except KeyboardInterrupt:
             done = True
             sim.client.close()
